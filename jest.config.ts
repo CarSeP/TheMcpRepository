90ed674627892bdbc3e322b29547b9672f461ced
CarSeP <vjpd64@gmail.com>
2025-12-23 12:26:11 -0400

    chore: install jest and create script for testing

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.d.ts",
+    "!src/**/*.test.ts",
+    "!src/**/*.spec.ts",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  coverageDirectory: "coverage",
+  verbose: true,
+};
